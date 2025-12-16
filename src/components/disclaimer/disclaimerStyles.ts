648a194a2ab3ccda72cd72d49efeb2edf4eebb64
Alejandro Curiel <alejandro.ma.garcia@outlook.com>
2025-12-15 20:53:45 -0500

    Disclaimer styling added

--- src/components/disclaimer/disclaimerStyles.ts
+++ src/components/disclaimer/disclaimerStyles.ts
@@ -1,10 +1,36 @@
 import styled from 'styled-components';
 
 export const DisclaimerStyles = styled.div`
-    padding: 16px;
+    padding: 80px;
     font-size: 14px;
+    height: 75vh;
     
     p, ul {
-        margin-bottom: 10px;
+        margin-bottom: 15px;
+    }
+
+    li {
+        margin-left: 50px;
+        list-style-type: disc;
+        margin: 10px 0 0 50px;
+    }
+
+    h1 {
+        font-size: 24px;
+        margin-bottom: 20px;
+    }
+
+    a {
+        color: #0066cc;
+    }
+
+    .disclaimer_img-container {
+        text-align: center;
+
+        img {
+            margin-bottom: 20px;
+            width: 200px;
+            image-rendering: pixelated;
+        }
     }
 `;
