4e7df6572153abfe9e446053267d3c95d29f9401
Alejandro Curiel <alejandro.ma.garcia@outlook.com>
2025-12-15 20:54:16 -0500

    Footer modified
    Added icons for linkedin and github, as well as some small style changes to the footer

--- src/components/footer/footerStyles.ts
+++ src/components/footer/footerStyles.ts
@@ -2,26 +2,27 @@ import styled from 'styled-components';
 
 export const FooterStyles = styled.footer`
     width: 100%;
+    height: 150px;
     background-color: #333;
     display: flex;
-    justify-content: center;
+    justify-content: space-around;
     align-items: center;
     color: white;
     position: relative;
     bottom: 0;
     margin-top: 20px;
     font-size: 14px;
-    
-    box-shadow: 0 -2px 5px rgba(0, 0, 0, 0.1);
 
-    .footer_top-container {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        width: 90%;
-        max-width: 1200px;
-        padding: 10px 0;
-        margin-bottom: 10px;
-        font-size: 14px;
+
+    .footer_icons {
+        svg {
+            font-size: 50px;
+            cursor: pointer;
+            margin: 0 15px;
+
+            &:hover {
+                color: #5FC0ED;
+            }
+        }
     }
 `;
